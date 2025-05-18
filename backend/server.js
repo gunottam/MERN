2b37a0015768650abc6d66b734f899cff9a48fc8
Gunottam Maini <gunottammaini@gmail.com>
2025-05-18 09:23:42 +0530

    feat: added nodemon

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/products", (req, res) => {
+    
+});
+app.listen(2000, () => {
+    console.log(`Server running on port http://localhost:2000`);
+});
